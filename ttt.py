ee9929f1a84fa81f993b33708ec1a04224272488
marika.savarese <marik.savarese@gmail.com>
2018-02-05 21:26:42 +0100

    clean up

--- ttt.py
+++
@@ -1,5 +0,0 @@
-from  model_lin_reg2 import Linear_regression
-url="http://markets.financialcontent.com/stocks/action/gethistoricaldata?Symbol=537%3A14917609&Month=1&Range=12&Year=2018"
-mydata = Linear_regression(url)
-table=mydata.MAE()
-print(table)
